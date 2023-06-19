290f0fc355f67171cf51eea7a562cce62fb5681c
Vishnu <vishnupv0808@gmail.com>
2023-06-19 16:49:35 +0530

    password connects

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const user_route = require('./routes/userRoute')
 const path = require('path')
 const admin_route = require('./routes/adminRoute')
-mongoose.connect('mongodb+srv://vishnu:<password>@cluster0.xwjes3g.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://vishnu:oagttOrKqg0FicuB@cluster0.xwjes3g.mongodb.net/?retryWrites=true&w=majority')
 const app = express()
 const myEnv = require('dotenv').config()
 
